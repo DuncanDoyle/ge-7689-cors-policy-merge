49cd4930029ade3db58faf265581f400f5883fac
Duncan Doyle <Duncan.Doyle@gmail.com>
2024-05-09 14:04:44 +0200

    Removed unused virtualservice from setup ...

--- install/setup.sh
+++ install/setup.sh
@@ -2,12 +2,7 @@
 
 pushd ..
 
-# Create httpbin namespace if it does not yet exist
-kubectl create namespace httpbin --dry-run=client -o yaml | kubectl apply -f -
-
-printf "\nDeploy HTTPBin service ...\n"
-kubectl apply -f apis/httpbin.yaml
-
+# Deploy the petstore application
 printf "\nDeploy Petstore application ...\n"
 kubectl apply -f apis/petstore.yaml
 
@@ -19,7 +14,6 @@ kubectl apply -f policies/cors-virtualhostoptions.yaml
 
 # VirtualServices
 printf "\nDeploy VirtualServices ...\n"
-kubectl apply -f virtualservices/api-example-com-vs.yaml
 kubectl apply -f virtualservices/petstore-example-com-vs.yaml
 
 popd
